1a510f5a6f55e3cdfa4ac56374295485e64c7788
tani <tani@1-10.com>
2021-01-29 16:35:17 +0900

    update next

---
+++ components/layout.js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utls.module.css'
+import Link from 'next/link'
+
+const name = 'tani'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="descrip" content="aaa" />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.jpg"
+              className={`${style.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <></>
+        )}
+      </header>
+      {children}
+    </div>
+  )
+}
