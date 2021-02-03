3decdde99c326d9ee3fdd3fc176719a9ade7b82c
tani <tani@1-10.com>
2021-02-03 16:50:15 +0900

    getStaticPaths

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,18 +1,31 @@
 import Layout from '@/components/layout'
+import { getAllPostIds, getPostData } from '@/lib/posts'
 
-export default function Post() {
-  return <Layout></Layout>
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title} <br />
+      {postData.id} <br />
+      {postData.date}
+    </Layout>
+  )
 }
 
 export async function getStaticPaths() {
   // idとしてとりうる値のリストを返す
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+export async function getStaticProps({ params }) {
+  // const allPostsData = getSortedPostsData()
+  const postData = getPostData(params.id)
   return {
     props: {
-      allPostsData,
+      postData,
     },
   }
 }
