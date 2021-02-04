01abc54e251df8bae2e826ebbd684acc64ef1502
tani <tani@1-10.com>
2021-02-04 10:20:45 +0900

    ids posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,22 @@
+import Head from 'next/head'
 import Layout from '@/components/layout'
 import { getAllPostIds, getPostData } from '@/lib/posts'
+import Date from '@/components/date'
+import utilStyles from '@/styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title} <br />
-      {postData.id} <br />
-      {postData.date}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
@@ -22,7 +32,8 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   // const allPostsData = getSortedPostsData()
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
+  console.log('postData', postData)
   return {
     props: {
       postData,
